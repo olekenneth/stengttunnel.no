99d19f9efe7717254ea1b2139cf34d89642fe5c8
Ole-Kenneth Bratholt <ok@rait.no>
2022-06-02 12:32:08 +0200

    Make sure to await user events and mock fetch requests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,44 @@ import user from "@testing-library/user-event";
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
+beforeEach(() => {
+  const makeFetchResponse = async (value) => ({ json: async () => value });
+  const mockFetch = jest
+    .fn()
+    .mockReturnValueOnce(
+      makeFetchResponse([
+        {
+          roadName: "Oslofjordtunnelen",
+          urlFriendly: "oslofjordtunnelen",
+          url: "https://api.stengttunnel.no/oslofjordtunnelen/status.json",
+        },
+        {
+          roadName: "Atlanterhavstunnelen",
+          urlFriendly: "atlanterhavstunnelen",
+          url: "https://api.stengttunnel.no/atlanterhavstunnelen/status.json",
+        },
+        {
+          roadName: "Blindheimstunnelen",
+          urlFriendly: "blindheimstunnelen",
+          url: "https://api.stengttunnel.no/blindheimstunnelen/status.json",
+        },
+      ])
+    )
+    .mockReturnValueOnce(
+      makeFetchResponse({
+        messages: [],
+        status: "green",
+        statusMessage: "Tunnelen ser ut til å være åpen.",
+        statusCode: 10,
+      })
+    );
+  global.fetch = mockFetch;
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
 test("Render app with Stengt tunnel in header", async () => {
   render(<App />);
 
@@ -14,12 +52,11 @@ test("Search for Oslofjordtunnelen and get status", async () => {
 
   await waitFor(() => screen.getByRole(/combobox/i));
   const dropdown = screen.getByRole(/combobox/i);
-  user.click(dropdown);
-  user.type(dropdown.querySelector("input"), "Oslofjord");
-
+  await user.click(dropdown);
+  await user.type(dropdown.querySelector("input"), "Oslofjord");
   await waitFor(() => screen.getByText(/Oslofjordtunnelen/i));
   const label = screen.getByText(/Oslofjordtunnelen/i);
-  user.click(label);
+  await user.click(label);
 
   await waitFor(() => screen.getByRole(/road/i));
   expect(screen.getByRole("status")).toHaveTextContent(
