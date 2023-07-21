de69f832c41f3780945bc25b300197a26c96ad20
Ole-Kenneth Bratholt <ok@schibsted.com>
2023-07-21 19:46:25 +0200

    Set default roads before fetching hopefully improve LCP 🤞 and remove GA

---
+++ src/utils.tsx
@@ -0,0 +1,227 @@
+export const defaultRoads = [
+  {
+    roadName: "Oslofjordtunnelen",
+    urlFriendly: "oslofjordtunnelen",
+    url: "https://api.stengttunnel.no/oslofjordtunnelen/status.json",
+  },
+  {
+    roadName: "Atlanterhavstunnelen",
+    urlFriendly: "atlanterhavstunnelen",
+    url: "https://api.stengttunnel.no/atlanterhavstunnelen/status.json",
+  },
+  {
+    roadName: "Blindheimstunnelen",
+    urlFriendly: "blindheimstunnelen",
+    url: "https://api.stengttunnel.no/blindheimstunnelen/status.json",
+  },
+  {
+    roadName: "Bjørgatunnelen",
+    urlFriendly: "bjorgatunnelen",
+    url: "https://api.stengttunnel.no/bjorgatunnelen/status.json",
+  },
+  {
+    roadName: "Bragernestunnelen",
+    urlFriendly: "bragernestunnelen",
+    url: "https://api.stengttunnel.no/bragernestunnelen/status.json",
+  },
+  {
+    roadName: "Brennetunnelen",
+    urlFriendly: "brennetunnelen",
+    url: "https://api.stengttunnel.no/brennetunnelen/status.json",
+  },
+  {
+    roadName: "Brynstunnelen",
+    urlFriendly: "brynstunnelen",
+    url: "https://api.stengttunnel.no/brynstunnelen/status.json",
+  },
+  {
+    roadName: "Byfjordtunnelen",
+    urlFriendly: "byfjordtunnelen",
+    url: "https://api.stengttunnel.no/byfjordtunnelen/status.json",
+  },
+  {
+    roadName: "Bømlafjordtunnelen",
+    urlFriendly: "bomlafjordtunnelen",
+    url: "https://api.stengttunnel.no/bomlafjordtunnelen/status.json",
+  },
+  {
+    roadName: "Eidfjordtunnelen",
+    urlFriendly: "eidfjordtunnelen",
+    url: "https://api.stengttunnel.no/eidfjordtunnelen/status.json",
+  },
+  {
+    roadName: "Eidsvolltunnelen",
+    urlFriendly: "eidsvolltunnelen",
+    url: "https://api.stengttunnel.no/eidsvolltunnelen/status.json",
+  },
+  {
+    roadName: "Eidsvågtunnelen",
+    urlFriendly: "eidsvagtunnelen",
+    url: "https://api.stengttunnel.no/eidsvagtunnelen/status.json",
+  },
+  {
+    roadName: "Eiksundtunnelen",
+    urlFriendly: "eiksundtunnelen",
+    url: "https://api.stengttunnel.no/eiksundtunnelen/status.json",
+  },
+  {
+    roadName: "Elgskauåstunnelen",
+    urlFriendly: "elgskauastunnelen",
+    url: "https://api.stengttunnel.no/elgskauastunnelen/status.json",
+  },
+  {
+    roadName: "Fannefjordtunnelen",
+    urlFriendly: "fannefjordtunnelen",
+    url: "https://api.stengttunnel.no/fannefjordtunnelen/status.json",
+  },
+  {
+    roadName: "Fosskollentunnelen",
+    urlFriendly: "fosskollentunnelen",
+    url: "https://api.stengttunnel.no/fosskollentunnelen/status.json",
+  },
+  {
+    roadName: "Gudvangatunnelen",
+    urlFriendly: "gudvangatunnelen",
+    url: "https://api.stengttunnel.no/gudvangatunnelen/status.json",
+  },
+  {
+    roadName: "Hanekleivtunnelen",
+    urlFriendly: "hanekleivtunnelen",
+    url: "https://api.stengttunnel.no/hanekleivtunnelen/status.json",
+  },
+  {
+    roadName: "Honningsvågtunnelen",
+    urlFriendly: "honningsvagtunnelen",
+    url: "https://api.stengttunnel.no/honningsvagtunnelen/status.json",
+  },
+  {
+    roadName: "Hvalertunnelen",
+    urlFriendly: "hvalertunnelen",
+    url: "https://api.stengttunnel.no/hvalertunnelen/status.json",
+  },
+  {
+    roadName: "Karmøytunnelen",
+    urlFriendly: "karmoytunnelen",
+    url: "https://api.stengttunnel.no/karmoytunnelen/status.json",
+  },
+  {
+    roadName: "Korgfjell tunnel",
+    urlFriendly: "korgfjell-tunnel",
+    url: "https://api.stengttunnel.no/korgfjell-tunnel/status.json",
+  },
+  {
+    roadName: "Knappetunnelen",
+    urlFriendly: "knappetunnelen",
+    url: "https://api.stengttunnel.no/knappetunnelen/status.json",
+  },
+  {
+    roadName: "Lyderhornstunnelen",
+    urlFriendly: "lyderhornstunnelen",
+    url: "https://api.stengttunnel.no/lyderhornstunnelen/status.json",
+  },
+  {
+    roadName: "Mastrafjordtunnelen",
+    urlFriendly: "mastrafjordtunnelen",
+    url: "https://api.stengttunnel.no/mastrafjordtunnelen/status.json",
+  },
+  {
+    roadName: "Matrebergtunnelen",
+    urlFriendly: "matrebergtunnelen",
+    url: "https://api.stengttunnel.no/matrebergtunnelen/status.json",
+  },
+  {
+    roadName: "Merraskottunnelen",
+    urlFriendly: "merraskottunnelen",
+    url: "https://api.stengttunnel.no/merraskottunnelen/status.json",
+  },
+  {
+    roadName: "Lærdalstunnelen",
+    urlFriendly: "laerdalstunnelen",
+    url: "https://api.stengttunnel.no/laerdalstunnelen/status.json",
+  },
+  {
+    roadName: "Nordbytunnelen",
+    urlFriendly: "nordbytunnelen",
+    url: "https://api.stengttunnel.no/nordbytunnelen/status.json",
+  },
+  {
+    roadName: "Nordkapptunnelen",
+    urlFriendly: "nordkapptunnelen",
+    url: "https://api.stengttunnel.no/nordkapptunnelen/status.json",
+  },
+  {
+    roadName: "Operatunnelen under Svartdalen",
+    urlFriendly: "operatunnelen-under-svartdalen",
+    url: "https://api.stengttunnel.no/operatunnelen-under-svartdalen/status.json",
+  },
+  {
+    roadName: "Ottatunnelen",
+    urlFriendly: "ottatunnelen",
+    url: "https://api.stengttunnel.no/ottatunnelen/status.json",
+  },
+  {
+    roadName: "Ryenlokket tunnel",
+    urlFriendly: "ryenlokket-tunnel",
+    url: "https://api.stengttunnel.no/ryenlokket-tunnel/status.json",
+  },
+  {
+    roadName: "Røneidtunnelen",
+    urlFriendly: "roneidtunnelen",
+    url: "https://api.stengttunnel.no/roneidtunnelen/status.json",
+  },
+  {
+    roadName: "Råumtunnelen",
+    urlFriendly: "raumtunnelen",
+    url: "https://api.stengttunnel.no/raumtunnelen/status.json",
+  },
+  {
+    roadName: "Skatestraumtunnelen",
+    urlFriendly: "skatestraumtunnelen",
+    url: "https://api.stengttunnel.no/skatestraumtunnelen/status.json",
+  },
+  {
+    roadName: "Skuitunnelen",
+    urlFriendly: "skuitunnelen",
+    url: "https://api.stengttunnel.no/skuitunnelen/status.json",
+  },
+  {
+    roadName: "Smestadtunnelen",
+    urlFriendly: "smestadtunnelen",
+    url: "https://api.stengttunnel.no/smestadtunnelen/status.json",
+  },
+  {
+    roadName: "Stampeleinåstunnelen",
+    urlFriendly: "stampeleinastunnelen",
+    url: "https://api.stengttunnel.no/stampeleinastunnelen/status.json",
+  },
+  {
+    roadName: "Stongafjelltunnelen",
+    urlFriendly: "stongafjelltunnelen",
+    url: "https://api.stengttunnel.no/stongafjelltunnelen/status.json",
+  },
+  {
+    roadName: "Strømsåstunnelen",
+    urlFriendly: "stromsastunnelen",
+    url: "https://api.stengttunnel.no/stromsastunnelen/status.json",
+  },
+  {
+    roadName: "Tromsøysundtunnelen",
+    urlFriendly: "tromsoysundtunnelen",
+    url: "https://api.stengttunnel.no/tromsoysundtunnelen/status.json",
+  },
+  {
+    roadName: "Tussviktunnelen",
+    urlFriendly: "tussviktunnelen",
+    url: "https://api.stengttunnel.no/tussviktunnelen/status.json",
+  },
+  {
+    roadName: "Vålerengtunnelen",
+    urlFriendly: "valerengtunnelen",
+    url: "https://api.stengttunnel.no/valerengtunnelen/status.json",
+  },
+  {
+    roadName: "Åkrafjordtunnelen",
+    urlFriendly: "akrafjordtunnelen",
+    url: "https://api.stengttunnel.no/akrafjordtunnelen/status.json",
+  },
+];
