9fbf448f65ea8c616166570512a200a8201eccf5
Ole-Kenneth Bratholt <ok@schibsted.com>
2025-02-09 19:53:50 +0100

    Remove web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,3 @@ import App from "./App";
 const root = createRoot(document.getElementById("root")!);
 
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
